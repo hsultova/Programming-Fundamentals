7f0dcfa86340d7d51bf35f7c5e469ab5d60d449d
hsultova <hsultova@gmail.com>
2016-05-19 17:32:55 +0300

    Updated ArrayTest.cs
    -Fixed command
    -PerformAction() - fixed position
    -Added lastElement and firstElement in ArrayShiftRight() and
    ArrayShiftLeft()

--- Git, GitHub, Debugging, Searching/ArrayTest.cs
+++ Git, GitHub, Debugging, Searching/ArrayTest.cs
@@ -16,11 +16,11 @@
                 .Select(long.Parse)
                 .ToArray();
 
-            string command = Console.ReadLine();
+            string line = Console.ReadLine();
 
-            while (!command.Equals("over"))
+            while (!line.Equals("stop"))
             {
-                string line = Console.ReadLine().Trim();
+                string command = line.Split(' ')[0];
                 int[] args = new int[2];
 
                 if (command.Equals("add") ||
@@ -31,22 +31,20 @@
                     args[0] = int.Parse(stringParams[0]);
                     args[1] = int.Parse(stringParams[1]);
 
-                    PerformAction(array, command, args);
                 }
 
                 PerformAction(array, command, args);
 
                 PrintArray(array);
-                Console.WriteLine('\n');
+                Console.WriteLine();
 
-                command = Console.ReadLine();
+                line = Console.ReadLine();
             }
         }
 
         static void PerformAction(long[] arr, string action, int[] args)
         {
-            long[] array = arr.Clone() as long[];
-            int pos = args[0];
+            int pos = args[0] - 1;
             int value = args[1];
 
             switch (action)
@@ -71,18 +69,24 @@
 
         private static void ArrayShiftRight(long[] array)
         {
+			long lastElement = array[array.Length - 1];
             for (int i = array.Length - 1; i >= 1; i--)
             {
                 array[i] = array[i - 1];
             }
+			array[0] = lastElement;
+			return array;
         }
 
         private static void ArrayShiftLeft(long[] array)
         {
+			long firstElement = array[0];
             for (int i = 0; i < array.Length - 1; i++)
             {
                 array[i] = array[i + 1];
             }
+			array[array.Length - 1] = firstElement;
+			return array;
         }
 
         private static void PrintArray(long[] array)
