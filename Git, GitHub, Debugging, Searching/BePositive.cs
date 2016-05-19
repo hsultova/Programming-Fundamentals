885562fbb44c7dd8876e0eae57fc19179645386f
hsultova <hsultova@gmail.com>
2016-05-19 15:06:23 +0300

    Updated BePositive.cs
    Repaired:
    - adding numbers to the list
    -added check for equality when currentNum is checked to be positive
    -catch situation when last number is negative
    (j != numbers.Count - 1)
    -added empty line after the line with numbers

--- Git, GitHub, Debugging, Searching/BePositive.cs
+++ Git, GitHub, Debugging, Searching/BePositive.cs
@@ -7,62 +7,55 @@
     {
         public static void Main()
         {
-            int countSequences = int.Parse(Console.ReadLine());
+           int countSequences = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < countSequences; i++)
-            {
-                string[] input = Console.ReadLine().Trim().Split(' ');
-                var numbers = new List<int>();
+			for (int i = 0; i < countSequences; i++)
+			{
+				string[] input = Console.ReadLine().Trim().Split(' ');
+				var numbers = new List<int>();
 
-                for (int j = 0; j < input.Length; j++)
-                {
-                    if (!input[j].Equals(string.Empty))
-                    {
-                        int num = int.Parse(input[i]);
-                        numbers.Add(num);
-                    }
-                }
+				for (int j = 0; j < input.Length; j++)
+				{
+					if (!input[j].Equals(string.Empty))
+					{
+						int num = int.Parse(input[j]);
+						numbers.Add(num);
+					}
+				}
 
-                bool found = false;
+				bool found = false;
 
-                for (int j = 0; j < numbers.Count; j++)
-                {
-                    int currentNum = numbers[j];
+				for (int j = 0; j < numbers.Count; j++)
+				{
+					int currentNum = numbers[j];
 
-                    if (currentNum > 0)
-                    {
-                        if (found)
-                            {
-                                Console.Write(" ");
-                            }
+					if (currentNum >= 0)
+					{
+						Console.Write(currentNum + " ");
+						found = true;
+					}
+					else if (j != numbers.Count - 1)
+					{
+						currentNum += numbers[j + 1];
 
-                            Console.Write(currentNum);
+						if (currentNum >= 0)
+						{
+							Console.Write(currentNum + " ");
+							found = true;
+						}
+						j++;
+					}
+				}
 
-                            found = true;
-                    }
-                    else
-                    {
-                        currentNum += numbers[j + 1];
-
-                        if (currentNum > 0)
-                        {
-                            if (found)
-                            {
-                                Console.Write(" ");
-                            }
-
-                            Console.Write(currentNum);
-
-                            found = true;
-                        }
-                    }
-                }
-
-                if (!found)
-                {
-                    Console.WriteLine("(empty)");
-                }
-            }
+				if (!found)
+				{
+					Console.WriteLine("(empty)");
+				}
+				else
+				{
+					Console.WriteLine();
+				}
+			}
         }
     }
 }
