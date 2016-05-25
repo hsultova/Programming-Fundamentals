64b6ad212747e883b0ab94036f5b81aced217934
hsultova <hsultova@gmail.com>
2016-05-25 15:09:49 +0300

    Added finished problems Arrays

---
+++ Arrays/DayOfWeek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgrammingBasicExam
+{
+	class  DayOfWeek
+	{
+		static void Main(string[] args)
+		{
+			int day = int.Parse(Console.ReadLine());
+			string[] daysOfWeeks = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+			if (day >= 1 && day <= 7)
+			{
+				Console.WriteLine(daysOfWeeks[day - 1]);
+			}
+			else
+			{
+				Console.WriteLine("Invalid day!");
+			}
+		}
+	}
+}
---
+++ Arrays/LastKNumbersSums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrammingFundamentals
+{
+	class  LastKNumbersSums
+	{
+		static void Main(string[] args)
+		{
+			var n = int.Parse(Console.ReadLine());
+			var k = int.Parse(Console.ReadLine());
+
+			var sequence = new long[n];
+			sequence[0] = 1;
+
+			for (int i = 1; i < n; i++)
+			{
+				var start = Math.Max(0, i - k);
+				var end = i - 1;
+				for (int j = start; j <= end; j++)
+				{
+					sequence[i] += sequence[j];
+				}
+			}
+			Console.WriteLine(string.Join(" ", sequence));
+		}
+	}
+}
