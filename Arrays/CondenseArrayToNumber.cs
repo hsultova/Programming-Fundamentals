36be7c7738e04995f03b1bb45f616a56a41292eb
hsultova <hsultova@gmail.com>
2016-05-25 20:06:44 +0300

    Added finished problems - Arrays

---
+++ Arrays/CondenseArrayToNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgrammingFundamentals
+{
+	class ArrayToNumber
+	{
+		static void Main(string[] args)
+		{
+			var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			int[] result = AddNumbers(numbers);
+
+			while (result.Length > 1)
+			{
+				result = AddNumbers(result);
+			}
+
+			Console.WriteLine(result[0]);
+		}
+
+		static int[] AddNumbers(int[] numbers)
+		{
+			int[] condensed = new int[numbers.Length - 1];
+			for (int i = 0; i < numbers.Length - 1; i++)
+			{
+				condensed[i] = numbers[i] + numbers[i + 1];
+			}
+			return condensed;
+		}
+	}
+}
---
+++ Arrays/ExtractMiddleElements.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgrammingFundamentals
+{
+	class MiddleElements
+
+	{
+		static void Main(string[] args)
+		{
+			var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			var n = numbers.Length;
+
+			if (n == 1)
+			{
+				Console.WriteLine("{ " + numbers[0] + " }");
+			}
+			else if (n % 2 == 0)
+			{
+				Console.WriteLine("{ " + numbers[n / 2 - 1] + ", " + numbers[n / 2] + " }");
+			}
+			else
+			{
+				Console.WriteLine("{ " + numbers[n / 2 - 1] + ", " + numbers[n / 2] + ", " + numbers[n / 2 + 1] + " }");
+			}
+		}
+	}
+}
