5e3a0b9c3fb3801d85c2c0ddb17b7b85185d6676
hsultova <hsultova@gmail.com>
2016-05-26 18:47:42 +0300

    Added EqualSums.cs - Arrays

---
+++ Arrays/EqualSums.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgrammingFundamentals
+{
+	class EqualSums
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			int leftSum;
+			int rightSum;
+			int j;
+			for (int i = 1; i < numbers.Length - 1; i++)
+			{
+				leftSum = 0;
+				rightSum = 0;
+				j = 0;
+				while (j < numbers.Length - 1)
+				{
+					if (j < i)
+					{
+						leftSum += numbers[i - j - 1];
+					}
+					if (j >= i)
+					{
+						rightSum += numbers[i + j - 1];
+					}
+					j++;
+				}
+				if (leftSum == rightSum)
+				{
+					Console.WriteLine(i);
+					return;
+				}
+			}
+			if (numbers.Length == 1 || numbers[1] == 0)
+			{
+				Console.WriteLine("0");
+				return;
+			}
+
+			Console.WriteLine("no");
+		}
+	}
+}
