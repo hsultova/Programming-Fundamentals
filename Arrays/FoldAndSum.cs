f1af8ea526039ca38a63b64520ce9289ce33d460
hsultova <hsultova@gmail.com>
2016-05-27 12:29:17 +0300

    Added finished problems Arrays

---
+++ Arrays/FoldAndSum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgrammingFundamentals
+{
+	class FoldAndSum
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			int n = numbers.Length;
+			int[] upper = new int[n / 2];
+			int[] lower = new int[n / 2];
+
+			int index = 0;
+			for (int i = n / 4 - 1; i >= 0; i--)
+			{
+				upper[index] = numbers[i];
+				index++;
+			}
+			for (int i = n - 1; i >= 3 * n / 4; i--)
+			{
+				upper[index] = numbers[i];
+				index++;
+			}
+			index = 0;
+			for (int i = n / 4; i < 3 * n / 4; i++)
+			{
+				lower[index] = numbers[i];
+				index++;
+			}
+			Console.WriteLine(string.Join(" ", Sum(upper, lower)));
+		}
+
+		static int[] Sum(int[] first, int[] second)
+		{
+			int[] summedNumbers = new int[first.Length];
+			int i = 0;
+			while (i < first.Length)
+			{
+				summedNumbers[i] = first[i] + second[i];
+				i++;
+			}
+			return summedNumbers;
+		}
+	}
+}
---
+++ Arrays/PrimeNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ProgrammingFundamentals
+{
+	class PrimeNumbers
+	{
+		static void Main(string[] args)
+		{
+			var n = int.Parse(Console.ReadLine());
+			bool[] primes = new bool[n + 1];
+			for (int i = 0; i < n; i++)
+			{
+				primes[i] = true;
+			}
+			primes[0] = false;
+			primes[1] = false;
+
+			//Fill prime numbers
+			for (int i = 2; i <= n; i++)
+			{
+				if (primes[i])
+				{
+					for (int j = 2 * i; j < primes.Length; j += i)
+					{
+						primes[j] = false;
+					}
+				}
+			}
+
+			//Print prime numbers
+			for (int i = 0; i < primes.Length; i++)
+			{
+				if (primes[i] == true)
+				{
+					if (i != primes.Length - 1)
+					{
+						Console.Write(i + " ");
+					}
+					else
+					{
+						Console.Write(i);
+					}
+				}
+			}
+		}
+	}
+}
