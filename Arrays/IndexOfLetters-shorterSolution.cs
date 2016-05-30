cb5c3724544bc8f643bd851997587131c7e96794
hsultova <hsultova@gmail.com>
2016-05-30 11:46:21 +0300

    Added finished problems Arrays

---
+++ Arrays/IndexOfLetters-shorterSolution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProgrammingFundamentals
+{
+	class  IndexOfLetters
+	{
+		static void Main(string[] args)
+		{
+			string word = Console.ReadLine();
+
+			for (int i = 0; i < word.Length; i++)
+			{
+				int letterIndex = Convert.ToInt32(word[i]) - 97;
+				Console.WriteLine("{0} -> {1}", word[i], letterIndex);
+			}
+		}
+	}
+}
---
+++ Arrays/Pairs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgrammingFundamentals
+{
+	class Pairs
+	{
+		static void Main(string[] args)
+		{
+			var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			var difference = int.Parse(Console.ReadLine());
+
+			int count = 0;
+			int j = 0;
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				for (j = 0; j < i; j++)
+				{
+					if (Math.Abs(numbers[i] - numbers[j]) == difference)
+					{
+						count++;
+					}
+				}
+			}
+
+			Console.WriteLine(count);
+		}
+	}
+}
