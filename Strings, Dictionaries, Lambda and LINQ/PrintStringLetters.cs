113dfa0889493a7df270895ce2de1f66255da5ac
hsultova <hsultova@gmail.com>
2016-06-04 13:18:28 +0300

    Added problem Strings

---
+++ Strings, Dictionaries, Lambda and LINQ/PrintStringLetters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProgrammingFundamentals
+{
+	class StringLetters
+	{
+		static void Main(string[] args)
+		{
+			var word = Console.ReadLine();
+
+			for (int i = 0; i < word.Length; i++)
+			{
+				Console.WriteLine("str[{0}] -> '{1}'", i, word[i]);
+			}
+		}
+	}
+}
