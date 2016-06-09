116aa6a3d4124ca3ac5a935afd11079f5a308f9e
hsultova <hsultova@gmail.com>
2016-06-09 21:44:21 +0300

    Added finished problems Dictionaries

---
+++ Strings, Dictionaries, Lambda and LINQ/PhonebookUpgrade.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrammingFundamentals
+{
+	class PhonebookUpgrade
+	{
+		static void Main(string[] args)
+		{
+			var line = Console.ReadLine();
+			var phonebook = new SortedDictionary<string, string>();
+			while (line != "END")
+			{
+				var tokens = line.Split(' ');
+				string command = tokens[0];
+
+
+				if (command == "A")
+				{
+					string name = tokens[1];
+					string phone = tokens[2];
+					if (!phonebook.ContainsKey(name))
+					{
+						phonebook.Add(name, phone);
+					}
+					else
+					{
+						phonebook[name] = phone;
+					}
+				}
+				else if (command == "S")
+				{
+					string name = tokens[1];
+					if (phonebook.ContainsKey(name))
+					{
+						Console.WriteLine($"{name} -> {phonebook[name]}");
+					}
+					else
+					{
+						Console.WriteLine($"Contact {name} does not exist.");
+					}
+				}
+				else if (command == "ListAll")
+				{
+					foreach (var record in phonebook)
+					{
+						Console.WriteLine($"{record.Key} -> {record.Value}");
+					}
+				}
+				line = Console.ReadLine();
+			}
+		}
+	}
+}
