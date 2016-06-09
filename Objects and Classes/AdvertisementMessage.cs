bff768f0cc463216f784915bc911e0835713cbd6
hsultova <hsultova@gmail.com>
2016-06-09 21:44:01 +0300

    Added finished problems Objects and Classes

---
+++ Objects and Classes/AdvertisementMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProgrammingFundamentals
+{
+	class AdvertisementMessage
+	{
+		static void Main(string[] args)
+		{
+			var n = int.Parse(Console.ReadLine());
+
+			var phrases = new string[] { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product." };
+			var events = new string[] { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+			var authors = new string[] { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+			var towns = new string[] { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+			Random random = new Random();
+			var phraseIndex = 0;
+			var eventIndex = 0;
+			var authorIndex = 0;
+			var townIndex = 0;
+
+			for (int i = 0; i < n; i++)
+			{
+				phraseIndex = random.Next(0, phrases.Length);
+				eventIndex = random.Next(0, events.Length);
+				authorIndex = random.Next(0, authors.Length);
+				townIndex = random.Next(0, towns.Length);
+				Console.WriteLine($"{phrases[phraseIndex]} {events[eventIndex]} {authors[authorIndex]} - {towns[townIndex]}");
+			}
+		}
+	}
+}
---
+++ Objects and Classes/CirclesIntersection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgrammingFundamentals
+{
+	class Point
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public double CalcDistance(Point p)
+		{
+			var x = Math.Abs(X - p.X);
+			var y = Math.Abs(Y - p.Y);
+			double distance = Math.Sqrt(x * x + y * y);
+			return distance;
+		}
+	}
+
+	class Circle
+	{
+		public Point Center { get; set; }
+		public int Radius { get; set; }
+
+		public bool Intersect(Circle circle)
+		{
+			var distance = Center.CalcDistance(circle.Center);
+			var sumRadius = Radius + circle.Radius;
+			if (distance <= sumRadius)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	class CirclesIntersection
+	{
+		static void Main(string[] args)
+		{
+			var circle1 = ReadCircle();
+			var circle2 = ReadCircle();
+
+			if (circle1.Intersect(circle2))
+			{
+				Console.WriteLine("Yes");
+			}
+			else
+			{
+				Console.WriteLine("No");
+			}
+		}
+
+		static Circle ReadCircle()
+		{
+			var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			return new Circle() { Center = new Point() { X = input[0], Y = input[1] }, Radius = input[2] };
+		}
+	}
+}
