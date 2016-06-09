754cf694d900649c4713014b1a652bdc322d41ac
hsultova <hsultova@gmail.com>
2016-06-09 15:05:07 +0300

    Added finished problems Strings, Dictionaries

---
+++ Strings, Dictionaries, Lambda and LINQ/CommonStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrammingFundamentals
+{
+	class CommonStrings
+	{
+		static void Main(string[] args)
+		{
+			var A = Console.ReadLine();
+			var B = Console.ReadLine();
+			var a = "";
+
+			for (int i = 1; i < A.Length; i++)
+			{
+				a = A.Substring(0, i);
+				if (B.Contains(a))
+				{
+					Console.WriteLine("yes");
+					return;
+				}
+			}
+
+			Console.WriteLine("no");
+		}
+	}
+}
---
+++ Strings, Dictionaries, Lambda and LINQ/Phonebook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrammingFundamentals
+{
+	class Phonebook
+	{
+		static void Main(string[] args)
+		{
+			var line = Console.ReadLine();
+			var phonebook = new Dictionary<string, string>();
+			while (line != "END")
+			{
+				var tokens = line.Split(' ');
+				char command = char.Parse(tokens[0]);
+				string name = tokens[1];
+
+				if (command == 'A')
+				{
+					string phone = tokens[2];
+					if (!phonebook.ContainsKey(name))
+					{
+						phonebook.Add(name, phone);
+					}
+					else
+					{
+						phonebook[name] = phone;
+					}
+				}
+				else if (command == 'S')
+				{
+					if (phonebook.ContainsKey(name))
+					{
+						Console.WriteLine($"{name} -> {phonebook[name]}");
+					}
+					else
+					{
+						Console.WriteLine($"Contact {name} does not exist.");
+					}
+				}
+				line = Console.ReadLine();
+			}
+		}
+	}
+}
